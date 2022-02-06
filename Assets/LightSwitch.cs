10f52dd01af7ac9466f46b5f6d8a309f9235393c
Luis Felipe Jimenez <jimluisf@gmail.com>
2022-02-06 12:58:33 -0500

    adding project to repo

---
+++ Assets/LightSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    Light lightComponent;
+
+    void Start()
+    {
+        lightComponent = GetComponent<Light>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        lightComponent.enabled = true;
+    }
+}
